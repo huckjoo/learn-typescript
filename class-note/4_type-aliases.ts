5b7379b1d3cd79dad504f133a68a67b71ccc0c94
권혁주(Hyukjoo Kwon)/FE개발팀/11ST <hyukjoo@sk.com>
2022-12-10 18:43:50 +0900

    project: 전화번호부 어플리케이션

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -1,20 +1,22 @@
-interface Person {
-  name: string;
-  age: number;
+{
+  interface Person {
+    name: string;
+    age: number;
+  }
+
+  // type Person = {
+  //   name: string;
+  //   age: number;
+  // };
+
+  const hero: Person = {
+    name: 'hj',
+    age: 20,
+  };
+
+  type MyString = string;
+  const str: MyString = 'hello';
+
+  type Todo = { id: string; title: string; done: boolean };
+  function getTodo(todo: Todo) {}
 }
-
-// type Person = {
-//   name: string;
-//   age: number;
-// };
-
-const hero: Person = {
-  name: 'hj',
-  age: 20,
-};
-
-type MyString = string;
-const str: MyString = 'hello';
-
-type Todo = { id: string; title: string; done: boolean };
-function getTodo(todo: Todo) {}
